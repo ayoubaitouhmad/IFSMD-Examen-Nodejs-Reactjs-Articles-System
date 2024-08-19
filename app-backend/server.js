1b689548007441703fa37b077b0a481bbd33f10d
mrx <ayoubaithmad77@gmail.com>
2024-08-19 15:36:31 +0100

    implement login functionality : backend

--- app-backend/server.js
+++ app-backend/server.js
@@ -4,8 +4,11 @@ const cors = require('cors');
 require('dotenv').config();
 
 const postRoutes = require('./routes/postRoutes');
+const getConnection = require("./config/db");
+const {sign} = require("jsonwebtoken");
 
 
+const logger = require('pino')()
 
 
 
@@ -16,6 +19,29 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/api', postRoutes);
+
+app.post('/api/login', async  (req, res) => {
+    const { email, password } = req.body;
+
+
+
+    const connection = await getConnection();
+
+    const [rows] = await connection.execute(
+        `SELECT * FROM users WHERE email = ? and password= ?`,
+        [email ,  password]
+    );
+    if (rows.length === 0) return res.status(400).send('User not found');
+    const user = rows[0];
+    const token = sign({ id: user.id }, process.env.JWT_SECRET, {
+        expiresIn: '1h',
+    });
+    logger.info(token)
+    res.json({ token });
+
+
+});
+
 
 const PORT = process.env.PORT ;
 

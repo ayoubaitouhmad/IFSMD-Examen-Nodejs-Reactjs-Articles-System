f06afca2d560b4fd4fadeda491fe573dd7b32074
mrx <ayoubaithmad77@gmail.com>
2024-08-21 17:07:31 +0100

    clean up articles routes

--- app-backend/routes/postRoutes.js
+++ app-backend/routes/postRoutes.js
@@ -6,8 +6,6 @@ router.get('/articles', postController.getAllPosts);
 router.get('/articles/latest', postController.latestPosts);
 router.get('/articles/most-viewed-articles', postController.mostViewedArticles);
 router.get('/articles/:id', postController.findPost);
-// router.post('/posts', postController.createPost);
-// router.put('/posts/:id', postController.updatePost);
-// router.delete('/posts/:id', postController.deletePost);
+
 
 module.exports = router;
--- app-frontend/src/services/postService.js
+++ app-frontend/src/services/postService.js
@@ -1,11 +1,12 @@
-import axios from 'axios';
-const API_URL = 'http://localhost:1000/api/articles';
-const API_GET_POST_URL = 'http://localhost:1000/api/articles/';
-const API_GET_LATEST_ARTICLES = 'http://localhost:1000/api/articles/latest';
-const API_GET_MOST_VIEWED_ARTICLES = 'http://localhost:1000/api/articles/most-viewed-articles';
+import axiosInstance from "../utils/axios";
+
+const API_URL = '/articles';
+const API_GET_POST_URL = '/articles/';
+const API_GET_LATEST_ARTICLES = '/articles/latest';
+const API_GET_MOST_VIEWED_ARTICLES = '/articles/most-viewed-articles';
 export const getPosts = async (page = 1, limit = 5, date, search) => {
     try {
-        const response = await axios.get(API_URL, {
+        const response = await axiosInstance.get(API_URL, {
             params: {page, limit, date, search}
         });
         return response.data;
@@ -16,7 +17,7 @@ export const getPosts = async (page = 1, limit = 5, date, search) => {
 };
 export const getLatestPosts = async () => {
     try {
-        const response = await axios.get(API_GET_LATEST_ARTICLES);
+        const response = await axiosInstance.get(API_GET_LATEST_ARTICLES);
         return response.data;
     } catch (error) {
         console.error('Error fetching posts:', error);
@@ -25,7 +26,7 @@ export const getLatestPosts = async () => {
 };
 export const getMostViewedArticles = async () => {
     try {
-        const response = await axios.get(API_GET_MOST_VIEWED_ARTICLES);
+        const response = await axiosInstance.get(API_GET_MOST_VIEWED_ARTICLES);
         return response.data;
     } catch (error) {
         console.error('Error fetching posts:', error);
@@ -34,7 +35,7 @@ export const getMostViewedArticles = async () => {
 };
 export const getPost = async (id) => {
     try {
-        const response = await axios.get(API_GET_POST_URL + id);
+        const response = await axiosInstance.get(API_GET_POST_URL + id);
         return response.data;
     } catch (error) {
         console.error('Error fetching posts:', error);
@@ -43,7 +44,7 @@ export const getPost = async (id) => {
 };
 export const addPost = async (post) => {
     try {
-        const response = await axios.post(API_URL, post);
+        const response = await axiosInstance.post(API_URL, post);
         return response.data;
     } catch (error) {
         console.error('Error adding post:', error);

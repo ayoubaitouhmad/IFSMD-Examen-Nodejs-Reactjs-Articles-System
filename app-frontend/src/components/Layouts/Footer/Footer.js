e9f57ff8060a395b59400e1518a6e37923e9ea02
mrx <ayoubaithmad77@gmail.com>
2024-08-29 17:33:37 +0100

    refactor footer

--- app-frontend/src/components/Layouts/Footer/Footer.js
+++ app-frontend/src/components/Layouts/Footer/Footer.js
@@ -1,19 +1,33 @@
 import React from 'react';
 
 function Footer() {
-  return (
-      <div className="pt-5">
-          <footer className="blog-footer pt-5  border-top">
-              <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a
-                  href="https://twitter.com/mdo">@mdo</a>.</p>
-              <p>
-                  <a href="#">Back to top</a>
-              </p>
-          </footer>
+    return (
+        <div className="pt-5">
+            <footer className="blog-footer pt-5 border-top ">
+                <p>
+                    Copyright © 2009 - 2024 .
+                </p>
+                <p className="">BlogPost made by
+                    <a href="https://twitter.com/mrx" target="_blank" rel="noopener noreferrer"> MRX</a>.
+                </p>
 
-      </div>
+                <div className="social-icons mb-3">
+                    <a href="https://twitter.com/mrx" target="_blank" rel="noopener noreferrer" className="mx-2">
+                        <i className="fab fa-twitter"></i>
+                    </a>
+                    <a href="https://github.com/mrx" target="_blank" rel="noopener noreferrer" className="mx-2">
+                        <i className="fab fa-github"></i>
+                    </a>
+                    <a href="https://linkedin.com/in/mrx" target="_blank" rel="noopener noreferrer" className="mx-2">
+                        <i className="fab fa-linkedin"></i>
+                    </a>
+                </div>
 
-  );
+            </footer>
+        </div>
+
+
+    );
 }
 
 export default Footer;

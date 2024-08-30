d9695b7c71fe8c6d4218f2fc2426fd66fc2b9b15
mrx <ayoubaithmad77@gmail.com>
2024-08-30 01:05:15 +0100

    wip

--- app-backend/db/database.sql
+++ app-backend/db/database.sql
@@ -1,78 +1,93 @@
-
-
-create table file_document
+-- auto-generated definition
+create table files
 (
     id         bigint auto_increment
         primary key,
-    data       varchar(255) null,
-    file_name  varchar(255) null,
-    file_type  varchar(255) null,
-    created_at datetime(6)  null,
-    updated_at datetime(6)  null,
-    path       varchar(255) null
+    file_name  varchar(255)                         null,
+    file_type  varchar(255)                         null,
+    file_path  varchar(255)                         null,
+    storage    varchar(255)                         null,
+    created_at datetime default current_timestamp() null,
+    updated_at datetime default current_timestamp() null,
+    path       varchar(255)                         null
 );
 
 
-
-
+-- auto-generated definition
 create table users
 (
     id               bigint auto_increment
         primary key,
-    email            varchar(255) null,
-    name             varchar(20)  not null,
-    password         varchar(255) null,
-    role             varchar(255) null,
     username         varchar(20)  not null,
+    name             varchar(20)  not null,
+    bio              varchar(20)  null,
+    role             varchar(20)  null,
+    email            varchar(255) null,
+    password         varchar(255) null,
     profile_image_id bigint       null,
     created_at       datetime(6)  null,
     updated_at       datetime(6)  null,
-    constraint UK4unapofvpijp79n4j3sheoun7
-        unique (profile_image_id),
-    constraint FKl3qc0qmq8x3v29tlv6v1lf0k1
-        foreign key (profile_image_id) references file_document (id)
+    constraint FK__USERS_FILES
+        foreign key (profile_image_id) references files (id)
+            on update cascade on delete set null
 );
 
+
+-- auto-generated definition
 create table articles
 (
-    id           bigint auto_increment
+    id               bigint auto_increment
         primary key,
-    content      text         not null,
-    created_at   datetime(6)  null,
-    title        varchar(255) null,
-    updated_at   datetime(6)  null,
-    author_id    bigint       null,
-    description  varchar(255) null,
-    dates_format varchar(255) null,
-    constraint FKe02fs2ut6qqoabfhj325wcjul
+    title            varchar(255)         null,
+    description      varchar(255)         null,
+    content          text                 not null,
+    is_featured_blog tinyint(1) default 0 null,
+    author_id        bigint               null,
+    views            int        default 0 null,
+    article_image_id bigint               null,
+    created_at       datetime(6)          null,
+    updated_at       datetime(6)          null,
+    constraint FK_ARTICLES_FILES
+        foreign key (article_image_id) references files (id)
+            on update cascade on delete set null,
+    constraint FK__ARTICLES_USERS
         foreign key (author_id) references users (id)
+            on update cascade on delete set null
 );
 
 
-drop table  comments;
-create table comments
+
+CREATE TABLE categories
 (
-    id      bigint auto_increment
-        primary key,
-    comment longtext null,
-    article_id bigint,
-    author_id bigint,
-    constraint fk__comments_articles
-        foreign key (article_id) references articles (id),
-    constraint fk__comments_users
-        foreign key (author_id) references users (id)
+    id          bigint AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE article_category
+(
+    article_id  bigint,
+    category_id bigint,
+    PRIMARY KEY (article_id, category_id),
+    FOREIGN KEY (article_id) REFERENCES articles (id) ON DELETE CASCADE ,
+    FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
 );
 
 
-alter table articles
-    add views int default 0 null after author_id;
 
-alter table articles
-    add article_image_id BIGINT  null after views;
 
-ALTER TABLE articles
-    ADD CONSTRAINT `FK_ARTICLES_FILES`
-        FOREIGN KEY (`article_image_id`) REFERENCES `files`(`id`)
-            ON DELETE SET NULL
-            ON UPDATE CASCADE;
+INSERT INTO categories (name, description) VALUES
+                                               ('Technology', 'Articles related to the latest technology trends and innovations.'),
+                                               ('Science', 'Exploring discoveries and advancements in various fields of science.');
+
+
+
+
+
+
+INSERT INTO article_category (article_id, category_id) VALUES
+                                                           (1, 1),
+                                                           (1, 2);
 
--- app-frontend/src/components/Layouts/Footer/Footer.js
+++ app-frontend/src/components/Layouts/Footer/Footer.js
@@ -2,27 +2,66 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div className="pt-5">
+        <div className=" pt-5">
             <footer className="blog-footer pt-5 border-top ">
-                <p>
-                    Copyright © 2009 - 2024 .
-                </p>
-                <p className="">BlogPost made by
-                    <a href="https://twitter.com/mrx" target="_blank" rel="noopener noreferrer"> MRX</a>.
-                </p>
 
-                <div className="social-icons mb-3">
-                    <a href="https://twitter.com/mrx" target="_blank" rel="noopener noreferrer" className="mx-2">
-                        <i className="fab fa-twitter"></i>
-                    </a>
-                    <a href="https://github.com/mrx" target="_blank" rel="noopener noreferrer" className="mx-2">
-                        <i className="fab fa-github"></i>
-                    </a>
-                    <a href="https://linkedin.com/in/mrx" target="_blank" rel="noopener noreferrer" className="mx-2">
-                        <i className="fab fa-linkedin"></i>
-                    </a>
+
+                <div className="row">
+                    <div className="col-12 col-md">
+
+                        <p>
+                            Copyright © 2009 - 2024 .
+                        </p>
+                        <p className="">BlogPost made by
+                            <a href="https://twitter.com/mrx" target="_blank" rel="noopener noreferrer"> MRX</a>.
+                        </p>
+
+
+                        <div className="social-icons mb-3">
+                            <a href="https://twitter.com/mrx" target="_blank" rel="noopener noreferrer"
+                               className="mx-2">
+                                <i className="fab fa-twitter"></i>
+                            </a>
+                            <a href="https://github.com/mrx" target="_blank" rel="noopener noreferrer" className="mx-2">
+                                <i className="fab fa-github"></i>
+                            </a>
+                            <a href="https://linkedin.com/in/mrx" target="_blank" rel="noopener noreferrer"
+                               className="mx-2">
+                                <i className="fab fa-linkedin"></i>
+                            </a>
+                        </div>
+
+                    </div>
+                    <div className="col-6 col-md">
+                        <h5>Features</h5>
+                        <ul className="list-unstyled text-small">
+                            <li><a className="text-muted" href="#">Cool stuff</a></li>
+                            <li><a className="text-muted" href="#">Random feature</a></li>
+                            <li><a className="text-muted" href="#">Team feature</a></li>
+                            <li><a className="text-muted" href="#">Stuff for developers</a></li>
+                            <li><a className="text-muted" href="#">Another one</a></li>
+                            <li><a className="text-muted" href="#">Last time</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-6 col-md">
+                        <h5>Resources</h5>
+                        <ul className="list-unstyled text-small">
+                            <li><a className="text-muted" href="#">Resource</a></li>
+                            <li><a className="text-muted" href="#">Resource name</a></li>
+                            <li><a className="text-muted" href="#">Another resource</a></li>
+                            <li><a className="text-muted" href="#">Final resource</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-6 col-md">
+                        <h5>About</h5>
+                        <ul className="list-unstyled text-small">
+                            <li><a className="text-muted" href="#">Team</a></li>
+                            <li><a className="text-muted" href="#">Locations</a></li>
+                            <li><a className="text-muted" href="#">Privacy</a></li>
+                            <li><a className="text-muted" href="#">Terms</a></li>
+                        </ul>
+                    </div>
                 </div>
-
             </footer>
         </div>
 
@@ -31,3 +70,5 @@ function Footer() {
 }
 
 export default Footer;
+
+

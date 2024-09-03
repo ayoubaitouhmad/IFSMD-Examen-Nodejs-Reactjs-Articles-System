22500b7b2990b3393af31cd8c93e2fe8b42500f2
mrx <ayoubaithmad77@gmail.com>
2024-09-03 18:56:10 +0100

    add page helper functions

--- app-frontend/src/utils/page.js
+++ app-frontend/src/utils/page.js
@@ -1,7 +1,31 @@
 import {useEffect} from "react";
+import { useNavigate, useLocation } from 'react-router-dom';
+
 
 export  const usePageTitle = (title) => {
     useEffect(() => {
         document.title = title;
     }, [title]);
+};
+
+export const updateQueryParam = (key, value, navigate) => {
+    const queryParams = new URLSearchParams(Location.search);
+
+    if (value) {
+        queryParams.set(key, value);
+    } else {
+        queryParams.delete(key);
+    }
+
+    navigate(`?${queryParams.toString()}`);
+};
+
+export const getQueryString = (key,LocationSearch) => {
+    const queryParams = new URLSearchParams(LocationSearch);
+
+    if (queryParams.has(key)) {
+        return queryParams.get(key);
+    }
+    return null
+
 };
